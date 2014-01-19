7136e29b9a81fe9bbe012c41423e24fea2ce8b50
Zachary Thompson <zgthompson@gmail.com>
2014-01-19 09:19:49 -0800

    Working affine decipher

--- CS340/cipher/affine.py
+++ CS340/cipher/affine.py
@@ -1,16 +1,31 @@
 from cipher import Cipher
-from string import ascii_lowercase
+from text import Text
+from english import ENGLISH_LETTERS
 
 class Affine(Cipher):
     def __init__(self, text):
         super(Affine, self).__init__(text)
 
     def decipher(self):
-        """This will be fun"""
+        best_result = self.text
+        # go through all possible affine alphabets
+        for a in ( 1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25 ):
+            for b in range(26):
+                cur_cipher = Text( self.encipher(a, b, inverse=True) )
 
-    def encipher(self, a, b):
+                # if cur_cipher looks more like english than best_result, it becomes the best_result
+                if cur_cipher.english_score > best_result.english_score:
+                    best_result = cur_cipher
+
+        return str(best_result)
+
+    # Returns a string of ciphertext
+    def encipher(self, a, b, **kwargs):
         # create new alphabet based on parameter a, b
-        alphabet = self.create_alphabet(a, b)
+        if kwargs and kwargs['inverse']:
+            alphabet = self.inverse_alphabet(a, b)
+        else:
+            alphabet = self.create_alphabet(a, b)
 
         # convert each character in text to cipher character
         # ord('a') = 97
@@ -20,5 +35,8 @@ class Affine(Cipher):
 
     def create_alphabet(self, a, b):
         # chr(97) = 'a'
-        return { ch: chr( 97 + (a * x + b) % 26) for x, ch in enumerate(ascii_lowercase) }
+        return { ch: chr( 97 + (a * x + b) % 26) for x, ch in enumerate(ENGLISH_LETTERS) }
+
+    def inverse_alphabet(self, a, b):
+        return { ch: chr( 97 + a * (x - b) % 26 ) for x, ch in enumerate(ENGLISH_LETTERS) }
 
--- CS340/cipher/text.py
+++ CS340/cipher/text.py
@@ -1,16 +1,31 @@
-from string import ascii_lowercase
+from english import ENGLISH_LETTERS, ENGLISH_FREQUENCY
+from math import sqrt
 
 class Text(object):
-    def __init__(self, text):
-        self.text = text.replace(" ", "").lower()
-        self.frequency = self.calculate_frequency()
 
-    def is_english(self):
-        """This will be fun"""
+
+    def __init__(self, text):
+        self.text = self.only_letters(text)
+        self.frequency = self.calculate_frequency()
+        self.english_score = 1 / self.english_deviation()
+
+    def english_deviation(self):
+        deviation = 0
+        for ch in ENGLISH_LETTERS:
+            deviation += (self.frequency[ch] - ENGLISH_FREQUENCY[ch])**2
+
+        return sqrt( deviation / 26 )
+
+
+    def only_letters(self, text):
+        # create dict for constant time lookup
+        lookup = { ch: True for ch in ENGLISH_LETTERS }
+
+        return "".join( [ ch for ch in text.lower() if ch in lookup ] )
 
     def calculate_frequency(self):
         # a dictionary of all characters equal to 0
-        ch_count = { ch: 0.0 for ch in ascii_lowercase }
+        ch_count = { ch: 0.0 for ch in ENGLISH_LETTERS }
 
         # increment the value of each character by one for each occurence
         for ch in self.text:
